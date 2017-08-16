efbdfe467b27d385c8648d88c1eea0b68c615758
hqxing <hqx2012000@163.com>
2017-08-16 16:25:16 +0800

    xiugai

---
+++ src/view/SignUp.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<div class="mui-input-row">
+			<label>用户名</label>
+			<input type="text" class="mui-input-clear" placeholder="带清除按钮的输入框">
+		</div>
+		<div class="mui-input-row">
+			<label>手机号</label>
+			<input type="text" class="mui-input-clear" placeholder="带清除按钮的输入框">
+		</div>
+		<div class="mui-input-row mui-password">
+			<label>密码</label>
+			<input type="password" class="mui-input-password">
+		</div>
+		<div class="mui-input-row mui-password">
+			<label>确认密码</label>
+			<input type="password" class="mui-input-password">
+		</div>
+	</div>
+</template>
